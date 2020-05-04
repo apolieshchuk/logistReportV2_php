5f530aa8dcb5f22702fac59af344c10d52a95687
APolieshchuk <anton2013ua@gmail.com>
2020-05-04 10:57:18 +0300

    add auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,15 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','AutosController@index')->name('auto.index');
-Route::post('/','AutosController@store')->name('auto.store');
-Route::get('/autos/data-load','AutosController@dataLoad')->name('auto.dataLoad');
-Route::get('/autos/{auto}','AutosController@show')->name('auto.show');
-Route::put('/autos/{auto}','AutosController@update')->name('auto.update');
-Route::delete('/autos/{auto}','AutosController@destroy')->name('auto.destroy');
-Route::get('/go','GoController@index')->name('go.index');
-Route::get('/report','ReportsController@index')->name('report.index');
-Route::delete('/report/{report}','ReportsController@destroy')->name('report.destroy');
-Route::get('/report/data-load','ReportsController@dataLoad')->name('report.dataLoad');
-Route::post('/report','ReportsController@store')->name('report.store');
-Route::get('/report/ratio','ReportsController@ratio')->name('report.ratio');
+Route::get('/','AutosController@index')
+    ->name('auto.index')->middleware('auth');
+Route::post('/','AutosController@store')
+    ->name('auto.store')->middleware('auth');
+Route::get('/autos/data-load','AutosController@dataLoad')
+    ->name('auto.dataLoad')->middleware('auth');
+Route::get('/autos/{auto}','AutosController@show')
+    ->name('auto.show')->middleware('auth');
+Route::put('/autos/{auto}','AutosController@update')
+    ->name('auto.update')->middleware('auth');
+Route::delete('/autos/{auto}','AutosController@destroy')
+    ->name('auto.destroy')->middleware('auth');
+Route::get('/go','GoController@index')
+    ->name('go.index')->middleware('auth');
+Route::get('/report','ReportsController@index')
+    ->name('report.index')->middleware('auth');
+Route::delete('/report/{report}','ReportsController@destroy')
+    ->name('report.destroy')->middleware('auth');
+Route::get('/report/data-load','ReportsController@dataLoad')
+    ->name('report.dataLoad')->middleware('auth');
+Route::post('/report','ReportsController@store')
+    ->name('report.store')->middleware('auth');
+Route::get('/report/ratio','ReportsController@ratio')
+    ->name('report.ratio')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
